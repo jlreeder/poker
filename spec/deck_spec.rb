6c9fd793a5bdc89e28a310e48ecc611299e85db0
Jason Reeder <info@jasonreeder.com>
2017-02-01 15:58:02 -0800

    Add specs for #initialize

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,11 +2,25 @@ require 'deck'
 
 describe 'Deck' do
   describe '#initialize' do
-    it 'has an array of cards'
+    subject { Deck.new.cards }
 
-    it 'has 52 cards'
+    it 'has an array of cards' do
+      expect(subject).to be_an(Array)
+    end
 
-    it 'has cards that are all unique'
+    it 'has 52 cards' do
+      expect(subject.length).to be(52)
+    end
+
+    it 'has cards that are all unique objects' do
+      expect(subject.uniq.length).to be(52)
+    end
+
+    it 'has cards that all have unique value-suit pairs' do
+      #NOTE: This is calling a method in Card. Should I refactor?
+      val_suits = subject.map { |card| card.show }
+      expect(val_suits.uniq.length).to be(52)
+    end
   end
 
   describe '#shuffle' do
