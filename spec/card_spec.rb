03275857929dbea48fd68f2c82fe6576f8125f21
Jason Reeder <info@jasonreeder.com>
2017-02-01 15:28:46 -0800

    Start specs for Card

---
+++ spec/card_spec.rb
@@ -0,0 +1,21 @@
+require 'card'
+
+describe 'Card' do
+  describe '#initialize' do
+    it 'has a suit'
+
+    it 'has a value'
+  end
+
+  describe '#match suit' do
+    it 'recognizes when it matches another card\'s suit'
+
+    it 'recognizes when it does not match another card\'s suit'
+  end
+
+  describe '#match value' do
+    it 'recognizes when it matches another card\'s value'
+
+    it 'recognizes when it does not match another card\'s value'
+  end
+end
