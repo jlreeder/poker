d24e0321bf81c50991571800f31c98f76ed6ea59
Jason Reeder <info@jasonreeder.com>
2017-02-01 17:19:12 -0800

    Add to Hand

---
+++ lib/hand.rb
@@ -0,0 +1,22 @@
+class Hand
+
+  HEIRARCHY = [ :straight_flush,
+                :four_of_a_kind,
+                :full_house,
+                :flush,
+                :straight,
+                :three_of_a_kind,
+                :two_pair,
+                :one_pair,
+                :high_card
+              ]
+
+  def initialize
+  end
+
+  def top_combo
+  end
+
+  private
+
+end
