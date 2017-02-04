965838a649773d08ba1fef50b321b3cbedc0525e
Jason Reeder <info@jasonreeder.com>
2017-02-04 15:59:59 -0800

    Debug and implement rank hashes

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,12 @@
 class Card
   SUITS_SYMS = %i(clubs diamonds hearts spades)
-  SUITS_NUMS = (0..4)
-  SUITS = Hash[Suits.zip(SUIT_VALS)]
+  SUITS_RANKS = (0..4)
+  SUITS = Hash[SUITS_SYMS.zip(SUITS_RANKS)]
 
   VALUES_SYMS = %i(two three four five six seven eight nine ten jack queen king
                    ace)
-  VALUES_VALS = (2..14)
-  VALUES = Hash[VALUES_SYMS.zip(VALUES_NUMS)]
+  VALUES_RANKS = (2..14)
+  VALUES = Hash[VALUES_SYMS.zip(VALUES_RANKS)]
 
 
   attr_reader :suit, :value
@@ -19,9 +19,19 @@ class Card
   end
 
   def <=>(other)
-    value_match = value <=> other.value
-    suit_match = suit <=> other.suit
+    value_match = value_rank <=> other.value_rank
+    suit_match = suit_rank <=> other.suit_rank
 
-    return 0 if value_match && suit_match
+    return 0 if value_match.zero? && suit_match.zero?
+  end
+
+  protected
+
+  def suit_rank
+    SUITS[@suit]
+  end
+
+  def value_rank
+    VALUES[@value]
   end
 end
