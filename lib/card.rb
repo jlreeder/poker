6cdef3e9cdfae1dd6e4aa834694da2a538bce900
Jason Reeder <info@jasonreeder.com>
2017-02-04 15:56:01 -0800

    Add value hashes
    So that values can be compared

--- lib/card.rb
+++ lib/card.rb
@@ -1,19 +1,26 @@
 class Card
-  SUITS = %i(spades clubs hearts diamonds)
-  VALUES = %i(two three four five six seven eight nine ten jack queen king ace)
+  SUITS_SYMS = %i(clubs diamonds hearts spades)
+  SUITS_NUMS = (0..4)
+  SUITS = Hash[Suits.zip(SUIT_VALS)]
+
+  VALUES_SYMS = %i(two three four five six seven eight nine ten jack queen king
+                   ace)
+  VALUES_VALS = (2..14)
+  VALUES = Hash[VALUES_SYMS.zip(VALUES_NUMS)]
+
 
   attr_reader :suit, :value
 
   def initialize(suit, value)
-    raise 'invalid suit' unless SUITS.include?(suit)
-    raise 'invalid value' unless VALUES.include?(value)
+    raise 'invalid suit' unless SUITS.key?(suit)
+    raise 'invalid value' unless VALUES.key?(value)
     @suit = suit
     @value = value
   end
 
   def <=>(other)
-    value_match = value == other.value
-    suit_match = suit == other.suit
+    value_match = value <=> other.value
+    suit_match = suit <=> other.suit
 
     return 0 if value_match && suit_match
   end
