8c0e678164467c88cd94fc3b6553b6a88810d75f
Jason Reeder <info@jasonreeder.com>
2017-02-04 15:43:53 -0800

    Start <=>

--- lib/card.rb
+++ lib/card.rb
@@ -5,9 +5,16 @@ class Card
   attr_reader :suit, :value
 
   def initialize(suit, value)
-    raise 'invalid suit' unless SUITS.contain?(suit)
-    raise 'invalid value' unless VALUES.contain?(value)
+    raise 'invalid suit' unless SUITS.include?(suit)
+    raise 'invalid value' unless VALUES.include?(value)
     @suit = suit
     @value = value
+  end
+
+  def <=>(other)
+    value_match = value == other.value
+    suit_match = suit == other.suit
+
+    return 0 if value_match && suit_match
   end
 end
