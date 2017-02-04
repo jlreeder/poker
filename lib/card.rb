cbfec9472005642a494c807d4ceb4426894c67d1
Jason Reeder <info@jasonreeder.com>
2017-02-04 15:38:01 -0800

    Raise error for invalid values

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,12 @@
 class Card
   SUITS = %i(spades clubs hearts diamonds)
+  VALUES = %i(two three four five six seven eight nine ten jack queen king ace)
 
   attr_reader :suit, :value
 
   def initialize(suit, value)
     raise 'invalid suit' unless SUITS.contain?(suit)
+    raise 'invalid value' unless VALUES.contain?(value)
     @suit = suit
     @value = value
   end
