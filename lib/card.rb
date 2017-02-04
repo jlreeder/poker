475cae9c889df82cc1b2da78768a28e1a4faa035
Jason Reeder <info@jasonreeder.com>
2017-02-04 15:34:39 -0800

    Add

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,11 @@
 class Card
+  SUITS = %i(spades clubs hearts diamonds)
+
+  attr_reader :suit, :value
+
+  def initialize(suit, value)
+    raise 'invalid suit' unless SUITS.contain?(suit)
+    @suit = suit
+    @value = value
+  end
 end
