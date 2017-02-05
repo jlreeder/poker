2ca151040f605dee77d9a6a9c54f167cf475d600
Jason Reeder <info@jasonreeder.com>
2017-02-04 17:43:41 -0800

    Add #trade_cards

--- lib/hand.rb
+++ lib/hand.rb
@@ -5,4 +5,14 @@ class Hand
     raise 'must have five cards' unless cards.length == 5
     @cards = cards
   end
+
+  def trade_cards(discard_cs, receive_cs)
+    raise 'must have five cards' unless discard_cs.length == receive_cs.length
+    raise 'cannot discard unowned card' unless discard_cs.all? do |c|
+      @cards.include?(c)
+    end
+
+    discard_cs.each { |card| @cards.delete(card) }
+    @cards.concat(receive_cs)
+  end
 end
