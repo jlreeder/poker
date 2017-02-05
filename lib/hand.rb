53183b1a50e9b3714245aa5955a2dd443fe6603c
Jason Reeder <info@jasonreeder.com>
2017-02-04 17:55:07 -0800

    Start hand rank logic

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,4 +1,17 @@
 class Hand
+  RANKS = [
+    :royal_flush,
+    :straight_flush,
+    :four_of_a_kind,
+    :full_house,
+    :flush,
+    :straight,
+    :three_of_a_kind,
+    :two_pair,
+    :one_pair,
+    :high_card
+  ]
+
   attr_reader :cards
 
   def initialize(cards)
@@ -15,4 +28,15 @@ class Hand
     discard_cs.each { |card| @cards.delete(card) }
     @cards.concat(receive_cs)
   end
+
+  def rank
+    :royal_flush
+  end
+
+  protected
+
+  def <=>(other)
+    RANKS.index(rank) <=> RANKS.index(other.rank)
+  end
+
 end
