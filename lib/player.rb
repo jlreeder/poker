8ac70c2b38c5cee5045afb9dcae2edea7b8f50e6
Jason Reeder <info@jasonreeder.com>
2017-02-05 12:59:29 -0800

    Add to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,22 @@
 class Player
-  attr_reader :bankroll
+  attr_reader :bankroll, :hand, :change
 
   def initialize(bankroll)
     @bankroll = bankroll
+    @hand = []
+    @bet = 0
+  end
+
+  def deal_in(hand)
+    @hand = hand
+  end
+
+  def take_bet(amount)
+    change = amount - @bet
+    raise 'not enough money' if bankroll - change < 0
+    @bet += amount
+    @bankroll -= change
+    change
   end
 
   def self.buy_in(bankroll)
