fdaba7b48d17421418b4339dd4d53307ff5555ca
Jason Reeder <info@jasonreeder.com>
2017-02-04 16:41:53 -0800

    Start Card

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,25 @@
 require 'card'
 
 class Deck
+  attr_reader :all_cards
+
+  def initialize(cards = Deck.default_deck)
+    @all_cards = cards
+  end
+
+  def count
+    @all_cards.count
+  end
+
+  def self.default_deck
+    deck = []
+
+    Card::SUITS_SYMS.each do |suit|
+      Card::VALUES_SYMS.each do |value|
+        deck << Card.new(suit, value)
+      end
+    end
+
+    deck
+  end
 end
