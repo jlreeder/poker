a99066503118fac1871b62a684a22e7d894cd475
Jason Reeder <info@jasonreeder.com>
2017-02-05 13:41:54 -0800

    Start Game

---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+require_relative 'deck'
+require_relative 'player'
+
+class Game
+  attr_reader :pot, :deck, :players
+
+  def initialize
+    @pot = 0
+    @deck = Deck.new
+    @players = []
+  end
+
+  def add_players(num, buy_in)
+    num.times { players << Player.new(buy_in) }
+  end
+end
